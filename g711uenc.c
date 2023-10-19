a390d969ca28000f7aa32fea84e4559cb43d5a10
helianchichi <william.turing@yahoo.com>
2023-10-19 15:10:06 +0800

    移位操作记得加括号，因为移位优先级低

--- g711uenc.c
+++ g711uenc.c
@@ -3,15 +3,15 @@
    
 unsigned char encode(short src)//-32768 ~ 32767
 {  
-    int sign = (src & 0x8000) >> 8; //1. 取符号位
+    int sign = ((src & 0x8000) >> 8); //1. 取符号位
     if(sign != 0)
         src = -src - 1;
     int Intensity_3bit = 7;
     int template = 0x4000; // 0100 0000 0000 0000
 
-    for(; (src & template) == 0 && Intensity_3bit >0; Intensity_3bit--,template >>= 1){}
+    for(; (src & template) == 0 && Intensity_3bit >0; Intensity_3bit--,(template >>= 1)){}
     int HighSample_4bit = (src >> (Intensity_3bit + 3)) & 0x0f;
-    unsigned char alaw = (unsigned char)(sign | Intensity_3bit << 4 | HighSample_4bit);
+    unsigned char alaw = (unsigned char)(sign | (Intensity_3bit << 4) | HighSample_4bit);
 
     return (unsigned char)(alaw ^0xFF); //1111 1111      
 } 
